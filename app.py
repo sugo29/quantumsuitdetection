3467b51190320cbf4cdc1c9f85fd77ed5607c2da
sugo29 <137184513+sugo29@users.noreply.github.com>
2025-07-18 10:11:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -148,4 +148,7 @@ def detect_image():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    
+    port = int(os.environ.get("PORT", 5000))  # fallback for local dev
+    app.run(host='0.0.0.0', port=port)
